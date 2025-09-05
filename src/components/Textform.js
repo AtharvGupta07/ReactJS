70ab8a9573fd1f9cc5f015185d041bdc084e1074
AtharvGupta07 <atharvgupta098@gmail.com>
2025-09-05 14:39:09 +0530

    Updating last work.

--- src/components/Textform.js
+++ src/components/Textform.js
@@ -10,6 +10,12 @@ export default function Textform(props) {
         setText(newText);
     }
 
+    const handleLoClick = ()=>{
+        console.log("Uppercase was clicked.");
+        let newText = text.toLowerCase();
+        setText(newText);
+    }
+
     const handleOnChange = (event )=>{
         // console.log("On change");
         setText(event.target.value);
@@ -19,13 +25,26 @@ export default function Textform(props) {
     // Wrong way to update text-> text = "xyz";
     // correct way -> setText("xyz")
   return (
-    <div>
+    <>
+    <div className='container'>
         <h3>{props.heading} </h3>
         <div className="mb-3">
         <textarea className="form-control" value={text}  onChange={handleOnChange}  id="myBox" rows="8"></textarea>
         </div>
-        <button className='btn btn-primary' onClick={handleUpClick}>Convert to upperCase</button>
+        <button className='btn btn-success' onClick={handleUpClick}>Convert to upperCase</button>
+        <button className='btn btn-danger mx-2' onClick={handleLoClick}>Convert to lowerCase</button>
     </div>
+
+    <div className='container my-3'>
+        <h3>Your text summary</h3>
+        <p> {text.split(" ").length} words and {text.length}characters</p>
+        <p>{0.008*text.split(" ").length} Minutes read</p>
+
+        <h3>Preview</h3>
+        <p>{text}</p>
+
+    </div>
+    </>
   )
 }
 
